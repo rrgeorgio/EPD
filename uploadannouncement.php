14ebe09d49f8606a73e847bc62d77e758391fec2
GEORGE <rrgeorgio@csd.auth.gr>
2020-12-28 20:56:26 +0200

    minor fix #2

--- uploadannouncement.php
+++ uploadannouncement.php
@@ -2,22 +2,19 @@
 
 session_start();
 
-
 date_default_timezone_set('Europe/Athens');
 $current_date=date('Y-m-d');
-$insert_post_sql_query=
+$insert_post_sql_query =
 "INSERT INTO `announcements`(`date`,`theme`,`text`)
 VALUES (\"".$current_date."\",\"".htmlentities($_POST["theme"],ENT_QUOTES)."\",\"".$_POST["content"]."\")
 "
 ;
 
-include "config.php";
-my_sqli_query($con,$insert_post_sql_query);
+$con = include "config.php";
+mysqli_query($con,$insert_post_sql_query);
 
 unset($_POST["theme"]); unset($_POST["content"]);
 echo("
     <script> alert(\"Επιτυχής δημιουργία ανακοίνωσης!\") </script>
     ");
-
-header("Location: announcement.php");
 ?>
