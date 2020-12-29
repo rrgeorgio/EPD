83b27f6abf092cdc1252f88fc12d330477901c7e
GEORGE <rrgeorgio@csd.auth.gr>
2020-12-29 16:50:41 +0200

    Fully functional file upload

--- uploaddocument.php
+++ uploaddocument.php
@@ -9,22 +9,22 @@ session_start();
     $fileError=$_FILES['fileToUpload']['error'];
     $extension= pathinfo($filename, PATHINFO_EXTENSION);
     if(!in_array($extension,['zip','docx','doc','pdf','odt'])){
-        echo ("<script> alert(\"Το έγγραφο πρέπει να είναι της μορφής zip,docx,doc,pdf η odt!\")</script>");
+        echo ("<script> alert(\"Το έγγραφο πρέπει να είναι της μορφής zip,docx,doc,pdf η odt!\");window.location.href='adddocument.php'</script>");
     }
     if($fileError === 0){
         if($fileSize < 1000000){
-            $filenamenew = uniqid('',true).".".$extension;
-            $destination = 'uploads/'.$filenamenew;
+            $destination = 'uploads/'.$filename;
             move_uploaded_file($filetmpname,$destination);
-            echo "success";
+            echo ("<script> alert(\"Το αρχείο μεταφορτώθηκε επιτυχώς\");window.location.href='documents.php';</script>");
+            
         
         } else{
-            echo ("<script> alert(\"Υπερβολικά μεγάλο μέγεθος αρχείου\")</script>");
+            echo ("<script> alert(\"Υπερβολικά μεγάλο μέγεθος αρχείου\");window.location.href='adddocument.php';</script>");
 
         }
 
     } else {
-        echo ("<script> alert(\"Σφάλμα κατά την μεταφόρτωση αρχείου\")</script>");
+        echo ("<script> alert(\"Σφάλμα κατά την μεταφόρτωση αρχείου\");window.location.href='adddocument.php'</script>");
     }
 
 
