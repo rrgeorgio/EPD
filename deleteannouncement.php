d7c9743394a5bfa474894ccbf76ab4697f2c29b5
GEORGE <rrgeorgio@csd.auth.gr>
2020-12-31 13:24:48 +0200

    Fixed id issues in delete announcement

--- deleteannouncement.php
+++ deleteannouncement.php
@@ -5,14 +5,25 @@
     $id="SELECT * FROM announcements ORDER BY id DESC LIMIT 0, 1";
     $result=mysqli_query($con,$id);
     $maxid=mysqli_fetch_array($result);
+    $total_pages="SELECT COUNT(*) FROM announcements";
+    $result=mysqli_query($con,$total_pages);
+    $total_rows=mysqli_fetch_array($result)[0];
+
+    
+    
 
     $delete_post="DELETE FROM announcements where id=\"".$_POST['postID']."\"";
     mysqli_query($con,$delete_post);
 
-    if($_POST['postID']==$maxid['id']){
-        echo 'OK';
-        $change_id="ALTER TABLE announcements AUTO_INCREMENT = ".$_POST['postID'];
+    if($total_rows==1){
+        echo "OK";
+        $change_id="ALTER TABLE announcements AUTO_INCREMENT = 1";
         mysqli_query($con,$change_id);
+    } else {
+        if($_POST['postID']==$maxid['id']){
+            $change_id="ALTER TABLE announcements AUTO_INCREMENT = ".$_POST['postID'];
+            mysqli_query($con,$change_id);
+            }
     }
-    header("Location:announcement.php");
     
+    header("Location:announcement.php");
